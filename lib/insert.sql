84f06a9e41ef82df030f8acb37980324d7930873
Albert Cheruiyot <bettalbert99@gmail.com>
2024-06-06 11:48:16 +0300

    First commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 8, 'M', 'Brown', 'Friendly', TRUE),
+  ('Rowdy', 5, 'M', 'Black', 'Aggressive', TRUE),
+  ('Tabitha', 10, 'F', 'Gray', 'Calm', TRUE),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Assertive', TRUE),
+  ('Melissa', 4, 'F', 'Honey', 'Playful', TRUE),
+  ('Grinch', 12, 'M', 'Green', 'Grumpy', TRUE),
+  ('Wendy', 7, 'F', 'White', 'Sociable', TRUE),
+  (NULL, NULL, NULL, NULL, NULL, NULL);
